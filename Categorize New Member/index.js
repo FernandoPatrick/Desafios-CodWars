0a8800172b5d9964a68ec12251e86c4725b65cb6
Fernando Patrick <fernando_lopes27@hotmail.com>
2022-06-02 15:16:07 -0400

    att

--- Categorize New Member/index.js
+++ Categorize New Member/index.js
@@ -1,7 +1,7 @@
 function openOrSenior(data){
   let result = []
   for(let i = 0; i < data.length; i++) {
-    if(data[i][0] >= 55 && data[i][1] >= 7) {
+    if(data[i][0] >= 55 && data[i][1] > 7) {
       result.push('Senior')
     }else {
       result.push('Open')
