2aecde9d331a2163a3636cc4dfebfcf9b88307f2
Fernando Patrick <fernando_lopes27@hotmail.com>
2022-06-03 08:59:00 -0400

    att

--- Sum without highest and lowest number/index.js
+++ Sum without highest and lowest number/index.js
@@ -1,14 +1,16 @@
 function sumArray(array) {
-  let soma = 0
-  let max = array.indexOf(Math.max(...array))
-  let min = array.indexOf(Math.min(...array))
-  console.log(max, min)
-  if(!array || array.length <= 2) return 0
-  for(let arr of array) {
-      soma += arr
-  }
-  soma = soma - array[max] - array[min] 
-  return soma
+  if (array == null) {
+   return 0;
+ }
+ let soma = 0
+ let max = array.indexOf(Math.max(...array))
+ let min = array.indexOf(Math.min(...array))
+ console.log(max, min)
+ if(!array || array.length <= 2) return 0
+ for(let arr of array) {
+     soma += arr
+ }
+ soma = soma - array[max] - array[min] 
+ return soma
 }
-
 console.log(sumArray([1,2,3,3,3,3]))
