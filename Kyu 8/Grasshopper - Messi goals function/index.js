8a846be25f0618a6cb8b1441e3a8b2a7a032cae8
Fernando Patrick <fernando_lopes27@hotmail.com>
2022-06-13 12:14:48 -0400

    att

---
+++ Kyu 8/Grasshopper - Messi goals function/index.js
@@ -0,0 +1,5 @@
+// Messi é um jogador de futebol com gols em três ligas: LaLiga Copa do Rei Campeões Complete a função para retornar seu número total de gols em todas as três ligas. Nota: a entrada será sempre válida. Por exemplo: 5, 10, 2  -->  17
+
+const goals = (a,b,c) => a + b + c
+
+console.log(goals(5,10,2)) // 17
