fffe56e96e36a5e32649aaa06252fb87ada30117
Fernando Patrick <fernando_lopes27@hotmail.com>
2022-06-21 08:43:17 -0400

    att

---
+++ Kyu 8/Número oposto/index.js
@@ -0,0 +1,13 @@
+// Opposite number
+
+// Muito simples, dado um inteiro ou um número de ponto flutuante, encontre seu oposto.
+
+// Exemplos:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+const opposite = (number) => number * -1 
+
+console.log(opposite(5)) // -5 
