c334aea12e9992fbdd0969793dbdcd33aec8ae0b
Fernando Patrick <fernando_lopes27@hotmail.com>
2022-06-29 18:18:00 -0400

    att

---
+++ Kyu 8/Ordem de lista inversa/index.js
@@ -0,0 +1,9 @@
+// Reverse List Order
+
+// Neste kata você criará uma função que recebe uma lista e retorna uma lista com a ordem inversa.
+
+// Exemplos (Entrada -> Saída)
+// * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// * [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+const reverseList = list => list.reverse()
